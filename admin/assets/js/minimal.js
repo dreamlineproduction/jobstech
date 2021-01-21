827d9d1bab5a4e6dccb3e810b47440198664a967
Dream Line Production <info@dreamlineproduction.in>
2021-01-22 01:57:45 +0530

    First commit

---
+++ admin/assets/js/minimal.js
@@ -0,0 +1,9 @@
+$('body').append('<section class="clearfix post_footer">@ Copyright Jobstech 2021-22. Dream Line Production. </section>');
+
+// $(document).ready(function(){
+
+// 	var purchase_code = $("#minimal").data("purchase-code");
+// 	var license_type = $("#license_type").data("license");
+// 	var website = $("#website").data("website");
+
+// });
