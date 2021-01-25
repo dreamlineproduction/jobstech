59efa648bf834da28d8fe0f9032e2a8070a521c1
dreamlineproduction <info@dreamlineproduction.in>
2021-01-25 18:52:30 +0530

    Working

--- screens/includes/proposal_sidebar.php
+++ screens/includes/proposal_sidebar.php
@@ -1,14 +1,16 @@
 <div class="card mb-5 rounded-0">
-<?php if($enableVideo == 0 and $proposal_price == 0){ include("sidebar/proposal_packages.php"); } ?>
-<div class="card-body order-box tab-content"><!--- card-body Starts --->
-  <?php 
+    <?php if($enableVideo == 0 and $proposal_price == 0){ include("sidebar/proposal_packages.php"); } ?>
+    <div class="card-body order-box tab-content">
+        <!--- card-body Starts --->
+        <?php 
    if($proposal_seller_vacation == "on"){ 
     include("sidebar/sellerVacationOn.php"); 
    }elseif($proposal_seller_vacation == "off"){
     include("sidebar/sellerVacationOff.php");
    }
   ?>
-</div><!--- card-body Ends --->
+    </div>
+    <!--- card-body Ends --->
 </div>
 <?php
   include('sidebar/referralBox.php');
--- screens/mobile/gigInfo/proposal_packages.php
+++ screens/mobile/gigInfo/proposal_packages.php
@@ -1,6 +1,6 @@
 <div class="card-header pt-0 pl-3 tabs-header">
-	<ul class="nav nav-tabs card-header-tabs rounded-0 justify-content-center">
-	<?php
+    <ul class="nav nav-tabs card-header-tabs rounded-0 justify-content-center">
+        <?php
 	$i=0;
 	$get_p = $db->select("proposal_packages",array("proposal_id"=>$proposal_id));
 	while($row = $get_p->fetch()){
@@ -8,11 +8,13 @@
 	$package_id = $row->package_id;
 	$package_name = $row->package_name;
 	?>
-	<li class="nav-item">
-		<a class="nav-link <?php if($package_name == "Standard"){ echo " active"; } ?>" href="#tab_<?= $package_id; ?>" style="font-size:19px; font-weight:350;" data-toggle="tab" formid="checkoutForm<?= $i; ?>">
-			<?= $lang['packages'][strtolower($package_name)]; ?>
-		</a>
-	</li>
-	<?php } ?>
-	</ul>
+        <li class="nav-item">
+            <a class="nav-link <?php if($package_name == "Standard"){ echo " active"; } ?>"
+                href="#tab_<?= $package_id; ?>" style="font-size:19px; font-weight:350;" data-toggle="tab"
+                formid="checkoutForm<?= $i; ?>">
+                <?= $lang['packages'][strtolower($package_name)]; ?>
+            </a>
+        </li>
+        <?php } ?>
+    </ul>
 </div>
