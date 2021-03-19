06f07f360f4fd53eec893608dbaeca0bdde98d99
dreamlineproduction <info@dreamlineproduction.in>
2021-03-20 02:38:12 +0530

    Added extra settings in video page for freelancer

--- plugins/videoPlugin/proposals/sections/edit/video.php
+++ plugins/videoPlugin/proposals/sections/edit/video.php
@@ -16,28 +16,42 @@ $row_delivery_time = $get_delivery_time->fetch();
 $delivery_proposal_title = $row_delivery_time->delivery_proposal_title;
 
 ?>
-<h4 class="font-weight-normal">Video</h4>
+<h4 class="font-weight-normal">Video Lesson Settings</h4>
 <hr>
 
 <form action="#" method="post" class="video-form"><!--- form Starts -->
 
   <div class="form-group row"><!--- form-group row Starts --->
     <label class="col-md-4 col-form-label"><?= $lang['enable_video_Lessons']; ?>:</label>
-    <div class="col-md-5">
+    <div class="col-md-8">
       <input type="checkbox" name="enable" class="mt-3" value="1" <?php if($enable==1){echo "checked";} ?>>
     </div>
   </div>
   
   <div class="form-group row"><!--- form-group row Starts --->
+  <label class="col-md-4 col-form-label">Price:</label>
+    <div class="col-md-8">
+      <input type="number" min="1" name="price_per_minute" class="form-control" value="<?=$price_per_minute; ?>" step="any" required="">
+    </div>
+  </div>
+
+  <!-- <div class="form-group row">
   <label class="col-md-4 col-form-label"><?= $lang['price_per_minute']; ?>:</label>
     <div class="col-md-5">
       <input type="number" min="0.5" name="price_per_minute" class="form-control" value="<?=$price_per_minute; ?>" step="any" required="">
+    </div>
+  </div> -->
+
+  <div class="form-group row"><!--- form-group row Starts --->
+    <label class="col-md-4 col-form-label">Maximum Number of Seat <small>Minmum 1, Maximum 12</small></label> 
+    <div class="col-md-8">
+    <input type="number" min="1" max="12"  name="price_per_minute" class="form-control" required>
     </div>
   </div>
 
   <div class="form-group row"><!--- form-group row Starts --->
   <div class="col-md-4 col-form-label"><?= $lang['label']['delivery_time']; ?></div>
-  <div class="col-md-5">
+  <div class="col-md-8">
   <select name="delivery_id" class="form-control" required="">
   <option value="<?= $d_delivery_id; ?>">  <?= $delivery_proposal_title; ?> </option>
   <?php 
@@ -55,7 +69,7 @@ $delivery_proposal_title = $row_delivery_time->delivery_proposal_title;
 
   <div class="form-group row"><!--- form-group row Starts --->
   <label class="col-md-4 col-form-label"><?= $lang['Days_within_which_a_video_session_can_be_scheduled']; ?>:</label>
-    <div class="col-md-5">
+    <div class="col-md-8">
       <select name="days_within_scheduled" class="form-control">
         <?php foreach($video_schedules as $schedule){ ?>
           <option value="<?= escape($schedule->id); ?>" <?php if($days_within_scheduled==$schedule->id){echo"selected";} ?>><?= escape($schedule->title); ?></option>
@@ -65,6 +79,294 @@ $delivery_proposal_title = $row_delivery_time->delivery_proposal_title;
   </div>
 
   <hr>
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="Sunday">
+    <label class="form-check-label" for="Sunday">Sunday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="monday">
+    <label class="form-check-label" for="monday">Monday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="Tuesday">
+    <label class="form-check-label" for="Tuesday">Tuesday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="Wednesday">
+    <label class="form-check-label" for="Wednesday">Wednesday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="Thursday">
+    <label class="form-check-label" for="Thursday">Thursday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="Friday">
+    <label class="form-check-label" for="Friday">Friday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+
+<div class="row mb-3 d-flex align-items-center">
+  
+<div class="col-md-2">
+<div class="form-group">
+
+<div class="form-check">
+    <input type="checkbox" class="form-check-input1" id="Saturday">
+    <label class="form-check-label" for="Saturday">Saturday</label>
+  </div>
+  </div>
+</div>
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">Start Time</label>
+    <select class="form-control" id="video_start_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+<div class="col-md-5">
+
+<div class="form-group">
+    <label for="video_day">End Time</label>
+    <select class="form-control" id="video_end_time">
+    <?php for($i = 0; $i < 24; $i++): ?>
+  <option value="<?= $i; ?>"><?= $i % 12 ? $i % 12 : 12 ?>:00 <?= $i >= 12 ? 'pm' : 'am' ?></option>
+<?php endfor ?>
+      
+    </select>
+  </div>
+</div>
+
+
+</div>
+
+  
   <div class="form-group mb-0"><!--- form-group Starts --->
     <a href="#" class="btn btn-secondary float-left back-to-overview">Back</a>
     <input class="btn btn-success float-right" type="submit" value="Save & Continue">
@@ -120,6 +422,6 @@ $(document).ready(function(){
       }
     });
   });
-
 });
 </script>
+
