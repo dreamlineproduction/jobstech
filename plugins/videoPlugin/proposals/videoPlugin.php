1ad68410a762a95fb4eb7d9a3acb7a94fab64c74
dreamlineproduction <info@dreamlineproduction.in>
2021-04-07 22:12:36 +0530

    Working on video class order details page

--- plugins/videoPlugin/proposals/videoPlugin.php
+++ plugins/videoPlugin/proposals/videoPlugin.php
@@ -5,15 +5,16 @@
 <h3>
     Price:
     <span class="<?= ($lang_dir == "left" ? 'float-right' : '') ?> font-weight-normal">
-            <?= showPrice($proposal_price, $priceClass); ?>
-        </span>
+        <?= showPrice($proposal_price, $priceClass); ?>
+    </span>
 </h3>
 
 <div id="notAvailable" class="d-none">
     <?php echo $lang['seat-not-available'] ?>
 </div>
 
-<form method="post" action="../../checkout" id="checkoutForm" class="<?= ($lang_dir == "right" ? 'text-right' : '') ?> <?= $proposal_seller_user_name != @$_SESSION['seller_user_name'] ? 'd-none' : '' ?>">
+<form method="post" action="../../checkout" id="checkoutForm"
+    class="<?= ($lang_dir == "right" ? 'text-right' : '') ?> <?= $proposal_seller_user_name != @$_SESSION['seller_user_name'] ? 'd-none' : '' ?>">
     <input type="hidden" name="proposal_id" value="<?= $proposal_id; ?>">
     <input type="hidden" name="proposal_qty" value="1">
     <input type="hidden" name="class_date" id="classDate" value="">
